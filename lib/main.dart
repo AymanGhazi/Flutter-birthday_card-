691bdce60ea37d79b337d8054ea9bfc88c461bca
Ayman Gamal Ghazi <ayman@amcde.com>
2023-06-10 14:22:06 +0300

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(bithdayCard());
+}
+
+class bithdayCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xffd2bcd5),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/Birthdaycard.png"),
+          ),
+        ),
+      ),
+    );
+  }
+}
